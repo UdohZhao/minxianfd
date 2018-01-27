496d296ebc83ad162b11a2a9e8d5e2b5e1f7dcea
zhaojiangetcunjicom <zhaojian@getcunji.com>
2018-01-27 15:57:20 +0800

    zhaojia

--- application/index/controller/WxPay.php
+++ application/index/controller/WxPay.php
@@ -16,6 +16,7 @@ class WxPay extends Base
     // 微信支付
     public function pay()
     {
+
         // 统一下单请求地址
         $url = 'https://api.mch.weixin.qq.com/pay/unifiedorder';
         // 组装请求参数
@@ -24,6 +25,7 @@ class WxPay extends Base
         $param['nonce_str'] = get_rand_str();
         $param['body'] = '岷县房东置顶推广充值';
         $param['out_trade_no'] = get_rand_str();
+        $param['attach'] = '111';
         $param['total_fee'] = bcmul($this->total_fee, 100, 0);
         $param['spbill_create_ip'] = $_SERVER['REMOTE_ADDR'];
         $param['notify_url'] = 'https://ngrok.getcunji.com/WxPay/notify';
@@ -56,8 +58,30 @@ class WxPay extends Base
     // 异步通知
     public function notify()
     {
-        slog($_GET);
-        slog($_POST);
+
+        $xml = file_get_contents("php://input");
+        $data = FromXml($xml);
+
+        slog($data);
+
+        // 签名验证
+        $sign = ToUrlParams($data);
+        if ($sign == $data['sign'])
+        {
+            slog('签名验证成功！');
+        }
+        else
+        {
+            slog('签名验证失败！');
+        }
+
+        // 同步返回给微信
+        $reex['return_code'] = 'SUCCESS';
+        $reex['return_msg'] = '';
+        $reex = ToXml($reex);
+        slog($reex);
+        return $reex;
+
     }
 
 }
