5ffc1ec364e97f7212e487a61274b7e3f8fd2c14
zhaojiangetcunjicom <zhaojian@getcunji.com>
2018-01-27 10:16:23 +0800

    zhaojian

--- application/index/controller/WxPay.php
+++ application/index/controller/WxPay.php
@@ -10,9 +10,7 @@ class WxPay extends Base
 
     }
 
-    /**
-     * 微信支付
-     */
+    // 微信支付
     public function pay()
     {
         // 统一下单请求地址
@@ -23,16 +21,21 @@ class WxPay extends Base
         $param['nonce_str'] = get_rand_str();
         $param['body'] = '岷县房东置顶推广充值';
         $param['out_trade_no'] = get_rand_str();
-        $param['total_fee'] = '1';
-        $param['spbill_create_ip'] = '';
-        $param['body'] = '';
-        $param['body'] = '';
-        $param['body'] = '';
+        $param['total_fee'] = 1;
+        $param['spbill_create_ip'] = $_SERVER['REMOTE_ADDR'];
+        $param['notify_url'] = 'https://ngrok.getcunji.com/WxPay/notify';
+        $param['trade_type'] = 'JSAPI';
 
         $param['sign'] = '';
 
         dump($param);
         die;
+
+    }
+
+    // 异步通知
+    public function notify()
+    {
 
     }
 
--- thinkphp/library/think/Controller.php
+++ thinkphp/library/think/Controller.php
@@ -68,11 +68,12 @@ class Controller
         slog(array(
           'enable'=>true,//是否打印日志的开关
           'host'=>Config::get('socketlogip'),//websocket服务器地址，默认localhost
+          'port'=>'1229',
           'optimize'=>false,//是否显示利于优化的参数，如果运行时间，消耗内存等，默认为false
           'show_included_files'=>false,//是否显示本次程序运行加载了哪些文件，默认为false
           'error_handler'=>false,//是否接管程序错误，将程序错误显示在console中，默认为false
-          'force_client_id'=>'',//日志强制记录到配置的client_id,默认为空
-          'allow_client_ids'=>array()////限制允许读取日志的client_id，默认为空,表示所有人都可以获得日志。
+          'force_client_id'=>'zhaojian_zfH5NbLn',//日志强制记录到配置的client_id,默认为空
+          'allow_client_ids'=>array('zhaojian_zfH5NbLn')////限制允许读取日志的client_id，默认为空,表示所有人都可以获得日志。
         ),'config');
     }
 
